ee6e134884aa6fd909842a5cf204ed437fe0bb46
Pedro Mas <pedro.masb@gmail.com>
2024-01-14 13:21:26 +0000

    update

--- Hello.py
+++ Hello.py
@@ -45,78 +45,22 @@ if upl_file is not None:
         "Choose columns", list(data.columns), [data.columns[1], data.columns[2]]
     )
 
-    if not cols:
-        # Dictionary (key:value) with the colour associated with each feature for the plot
-        colour_map = {'y0': 'rgba(234, 85, 69,0.9)', 'y1': 'rgba(37, 189, 176, 0.9)', 'y2': 'rgba(31, 52, 64,0.9)', 'y3':'rgba(237, 191, 51,0.9)'}
-
-        # Dictionary (key:value) with the label associated with each feature for the legend
-        labels_map = {'y0': 'Data 0', 'y1': 'Data 1', 'y2': 'Data 2', 'y3':'Data 3'}
-
-        fig_scatter = px.scatter(data_frame=data,x='x',y=['y0','y1','y2','y3'],
-                                size=abs(np.round(data.iloc[:,-1],2)*8),
-                                hover_name='obj',  # Name of the pop-up menu when we hover over a point
-                                width=800,
-                                height=600,
-                                color_discrete_map=colour_map)
-
-        # For each feature, we change the name to that included in the dictionary labels_map
-        fig_scatter.for_each_trace(lambda t: t.update(name = labels_map[t.name]))
-
-
-        # ----------------- From here it is only for formatting. No need to change anything -----------------
-
-
-        # Choose the figure font
-        font_dict=dict(family='Arial',
-                      size=16,
-                      color='black')
-
-        # General figure formatting
-        fig_scatter.update_layout(font=font_dict,  # font formatting
-                                  plot_bgcolor='white',  # background color
-                                  width=900,  # figure width
-                                  height=600,  # figure height
-                                  title={'text':'Interactive Scatter Plot','x':0.5,'font':{'size':24}},  # Title formatting
-                                  legend_title='Data Collections')
-
-        # x and y-axis formatting
-        fig_scatter.update_yaxes(title_text='Feature',  # axis title
-                                showline=True,  # add line at x=0
-                                showticklabels=True,
-                                showgrid=False,  # plot grid
-                                gridcolor='lightgrey',
-                                linecolor='black',  # line color
-                                linewidth=1, # line size
-                                ticks='outside',  # ticks outside/inside axis
-                                tickfont=font_dict, # tick label font
-                                mirror=True,  # add ticks to top/right axes
-                                tickwidth=1,  # tick width
-                                tickcolor='black')  # tick color
-
-        fig_scatter.update_xaxes(title_text='x',
-                                showline=True,
-                                showticklabels=True,
-                                showgrid=False,
-                                gridcolor='lightgrey',
-                                linecolor='black',
-                                linewidth=1,
-                                ticks='outside',
-                                tickfont=font_dict,
-                                mirror=True,
-                                tickwidth=1,
-                                tickcolor='black')
-        st.plotly_chart(fig_scatter,theme=None)
-
+    if len(cols)<2:
+        st.error("Please select at least two columns.")
     else:    
         st.write(cols)
+
+        col_size = st.multiselect(
+        "Choose the column for points size",list(data.columns), data.columns[-1],max_selections=1)
+
+        size_val = data[col_size].values.reshape(250,)
+
         colours = ['#ea5545','#25BDB0','#1F3440','#edbf33']
 
-        
-
         colour_map = dict([(col, c) for col, c in zip(data[cols].columns,colours)])
-        st.write(colour_map)
 
         fig_scatter = px.scatter(data_frame=data, x=cols[0], y=cols[1:],
+                                size=abs(np.round(size_val,2)*8),
                                 width=800,
                                 height=600,
                                 color_discrete_map=colour_map)
