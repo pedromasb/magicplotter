6c52719fd311c7a40b07fc63c803e57e5a2cabb6
Pedro Mas <pedro.masb@gmail.com>
2024-05-22 16:01:46 +0000

    upd

--- pages/Scatter_Plot.py
+++ pages/Scatter_Plot.py
@@ -62,10 +62,9 @@ if upl_file is not None:
 
     cols = st.multiselect(
         "Choose columns", list(data.columns),  [data.columns[1], data.columns[2]], max_selections=5
-        )
-        
-    if len(cols)<2:
+    )
 
+    if len(cols)<2:
         st.error("Please select at least two columns.")
 
     else:    
