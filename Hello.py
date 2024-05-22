39df72bd438f53696fd564515cadf7d5bb1599ec
Pedro Mas <pedro.masb@gmail.com>
2024-05-22 10:58:59 +0000

    upd

--- Hello.py
+++ Hello.py
@@ -25,6 +25,22 @@ st.markdown('''
 
             With **MagicPlotter**, you will save up to **500 lines of code per visualization**. Interactive data-sharing has never been easier!
 
+            #### Streamlit's Data Privacy
+
+            Streamlit Cloud is [SOC 2 Type 1 compliant](https://blog.streamlit.io/streamlit-cloud-is-now-soc-2-type-1-compliant/), which means they have been audited and
+            found to have appropriate systems in place to ensure the security of the system (protecting against unauthorized access) and the confidentiality of the information 
+            processed by the system (ensuring that sensitive data is accessed only by authorized individuals) at a specific point in time.
+
+            Streamlit has a server-client structure. The server executes the Python code and the client is a browser, typically on a different computer from the server.
+            When you upload a file to the app, the data is sent from the client to the server and contained in a BytesIO buffer in Python memory (i.e. RAM, not disk).
+            As files are stored in memory, they get deleted immediately as soon as they're not needed anymore. This means Streamlit removes a file from memory when:
+
+            - The user uploads another file, replacing the original one.
+            - The user clears the file uploader.
+            - The user closes the browser tab where they uploaded the file.
+
+            Additional details can be found in their [Trust and Security](https://docs.streamlit.io/deploy/streamlit-community-cloud/get-started/trust-and-security) policy.
+
 
 
 
